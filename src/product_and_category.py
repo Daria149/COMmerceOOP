968035cb4316c14127a4c8108b4fb1468769aaeb
Daria149 <strelcova.dasha87@gmail.com>
2024-12-15 21:15:42 +0400

    feature_task_14_2

--- src/product_and_category.py
+++ src/product_and_category.py
@@ -1,3 +1,4 @@
+
 class Product:
     """Класс, определяющий характеристики продукта."""
 
@@ -10,8 +11,30 @@ class Product:
         """Метод для инициализации класса Product."""
         self.name = name
         self.description = description
-        self.price = price
+        self.__price = price
         self.quantity = quantity
+
+    @classmethod
+    def new_product(cls, parameters: dict):
+        return cls(**parameters)
+
+    @property
+    def price(self):
+        return self.__price
+
+    @price.setter
+    def price(self, new_price: int):
+        if int(new_price) <= 0:
+            print("Цена не должна быть нулевая или отрицательная")
+            return
+        elif self.__price > new_price:
+            approval = input("Новая цена ниже текущей. Подтвердите: подтверждаете: 'y', не подтверждвете: 'n' ___")
+            if approval == "y" and new_price >= 0:
+                self.__price = new_price
+            else:
+                self.__price = self.__price
+        else:
+            self.__price = self.__price
 
 
 class Category:
@@ -28,6 +51,21 @@ class Category:
         """Метод для инициализации класса Category."""
         self.name = name
         self.description = description
-        self.products = products if products else []
+        self.__products = products if products else []
         self.category_count += 1
         self.product_count += len(products) if products else 0
+
+    def add_product(self, new_product: Product):
+        self.__products.append(new_product)
+        self.product_count += 1
+
+    @property
+    def products_list(self):
+        products_str = ""
+        for product in self.__products:
+            products_str += f"{product.name}, {product.price} руб. Остаток: {product.quantity} шт.\n"
+        return products_str
+
+    @property
+    def products(self):
+        return self.__products
--- tests/test_product_and_category.py
+++ tests/test_product_and_category.py
@@ -1,3 +1,5 @@
+from unittest.mock import patch
+
 import pytest
 
 from src.product_and_category import Category, Product
@@ -27,3 +29,35 @@ def test_3_category_fruit():
     category1 = Category(name="healthy fruit", description="good and fresh", products=["apple", "pear", "orange"])
     assert category1.category_count == 1
     assert category1.product_count == 3
+
+
+def test_new_product():
+    p = Product.new_product({"name": "Samsung", "description": "256GB, Серый цвет", "price": 50000.0, "quantity": 5})
+    assert (f"{p.name}, {p.description}, {p.price}, {p.quantity}") == "Samsung, 256GB, Серый цвет, 50000.0, 5"
+
+
+@patch("builtins.input")
+def test_price_setter(capsys, mock_input):
+    new_product = Product.new_product(
+        {"name": "Samsung", "description": "256GB, Серый цвет", "price": 100.0, "quantity": 5}
+    )
+    new_product.price = 50
+    message = capsys.readouterr()
+    assert message.out.strip() == "Новая цена ниже текущей. Подтвердите: подтверждаете: 'y', не подтверждвете: 'n' ___"
+    mock_input.return_value = "y"
+    assert new_product.price == 50
+
+    new_product.price = 0
+    message = capsys.readouterr()
+    assert message.out.strip() == "Цена не должна быть нулевая или отрицательная"
+
+    new_product.price = 5000.0
+    assert new_product.price == 5000.0
+
+
+def test_products_list():
+    product1 = Product("QLED 4K", "Фоновая подсветка", 123000.0, 7)
+    assert (
+        f"{product1.name}, {product1.price} руб. Остаток: {product1.quantity} шт.\n"
+        == "QLED 4K, 123000.0 руб. Остаток: 7 шт.\n"
+    )
